39f294f77bed675a36719c1c8cd94d27990f7d33
zhengchao <617069197@qq.com>
2020-06-07 20:05:59 +0800

    修正webpack配置

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,14 +1,16 @@
 module.exports = {
-    "plugins": {
-      // to edit target browsers: use "browserslist" field in package.json
-      "autoprefixer": {
-        "browsers": [
-          "ie >= 9",
-          "ff >= 30",
-          "chrome >= 34",
-          "safari >= 7",
-          "opera >= 23"
-        ]
-      }
+  plugins: {
+    // 兼容浏览器，添加前缀
+    'autoprefixer':{
+        overrideBrowserslist: [
+          "Android 4.1",
+          "iOS 7.1",
+          "Chrome > 31",
+          "ff > 31",
+          "ie >= 8"
+          //'last 10 versions', // 所有主流浏览器最近10版本用
+      ],
+      grid: true
     }
   }
+}
